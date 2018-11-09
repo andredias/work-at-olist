21789cd1d9b0a593cf50639b7bc8a704db0d04cf
André Felipe Dias <andre.dias@pronus.io>
2018-11-09 21:07:05 -0200

    Fixes a few PEP8 errors

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from marshmallow import post_dump
 from .. import ma
 from ..models import Call
@@ -10,7 +9,6 @@ ISO8601 = '%Y-%m-%dT%H:%M:%SZ'
 class CallSchema(ma.ModelSchema):
     class Meta:
         model = Call
-
 
     @post_dump
     def mk_iso8601_timestamp(self, data):
