b9cae490af12fbda7ce2bdf29082b825ad9096b4
André Felipe Dias <andre.dias@pronus.io>
2018-11-13 01:30:09 -0200

    prepare flask deploy procedure

--- olist.py
+++ olist.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from datetime import datetime
 from dotenv import load_dotenv
 
 dotenv_path = Path(__file__).parent / '.env'
@@ -16,11 +17,39 @@ migrate = Migrate(app, db)
 @app.cli.command()
 def deploy():
     """Run deployment tasks."""
-    # migrate database to latest revision
-    upgrade()
+    if 'sqlite://' in app.config['SQLALCHEMY_DATABASE_URI']:
+        # SQLite doesn't handle well with upgrade
+        path = Path(app.config['SQLALCHEMY_DATABASE_URI'][10:])
+        if path.is_file():
+            path.unlink()
+        db.create_all()
+    else:
+        # migrate database to latest revision
+        upgrade()
 
-    # create or update user roles
-    # Role.insert_roles()
+    # insert sample data
+    # this is part of olist's original requirements
+    from app.models import Call
+    source = '99988526423'
+    destination = '9933468278'
+    data = (
+        (70, datetime(2016, 2, 29, 12, 0, 0), datetime(2016, 2, 29, 14, 0, 0)),
+        (71, datetime(2017, 12, 11, 15, 7, 13), datetime(2017, 12, 11, 15, 14, 56)),
+        (72, datetime(2017, 12, 12, 22, 47, 56), datetime(2017, 12, 12, 22, 50, 56)),
+        (73, datetime(2017, 12, 12, 21, 57, 13), datetime(2017, 12, 12, 22, 10, 56)),
+        (74, datetime(2017, 12, 12, 4, 57, 13), datetime(2017, 12, 12, 6, 10, 56)),
+        (75, datetime(2017, 12, 13, 21, 57, 13), datetime(2017, 12, 14, 22, 10, 56)),
+        (76, datetime(2017, 12, 12, 15, 7, 58), datetime(2017, 12, 12, 15, 12, 56)),
+        (77, datetime(2018, 2, 28, 21, 57, 13), datetime(2018, 3, 1, 22, 10, 56)),
+    )
+    # bulk insert
+    # ref: https://docs.sqlalchemy.org/en/latest/_modules/examples/performance/bulk_inserts.html
+    db.session.add_all(
+        Call(id=v[0], source=source, destination=destination, start_timestamp=v[1],
+             end_timestamp=v[2]) for v in data
+    )
+    db.session.flush()
+    db.session.commit()
 
     # ensure all users are following themselves
     # User.add_self_follows()
